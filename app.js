089eb3f4b7129c827a668b60c2817f54e67609dd
Aidan Brown <aidan.brown@flextrade.com>
2016-08-19 16:29:45 +0100

    add websocket support

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var path = require('path');
 var bodyParser = require('body-parser');
 
@@ -9,6 +10,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    //io.emit('chat message', msg);
+    setInterval( function() {
+      io.emit('chat message', "Hello")
+    },1000)
+  });
 });
 
 var port = 10000;
