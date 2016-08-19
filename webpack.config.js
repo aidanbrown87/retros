0afae265c6ed65af44400cccd29790cf6dfb1b77
Aidan Brown <aidan.brown@flextrade.com>
2016-08-19 17:35:32 +0100

    set up React

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './src/client.js',
+  output: {
+    path: './public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json']
+  }
+};
